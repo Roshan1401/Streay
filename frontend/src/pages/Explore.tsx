61fd54d069b20e607152ea2631b03007197f1faf
Roshan Patil <patilroshan9687@gmail.com>
2026-06-27 14:23:35 +0530

    refactor: replace DevloperCard with UserSearchCard in Explore component

--- frontend/src/pages/Explore.tsx
+++ frontend/src/pages/Explore.tsx
@@ -1,4 +1,4 @@
-import DevloperCard from "../components/explore/DevloperCard";
+import UserSearchCard from "../components/explore/UserSearchCard";
 import Searchbar from "../components/explore/Searchbar";
 import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
@@ -9,7 +9,12 @@ function Explore() {
   const [query, setQuery] = useState("");
   const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
   const [showResults, setShowResults] = useState(false);
-  const { recentSearches, addRecentSearch, removeRecentSearch, clearRecentSearches } = useRecentSearchs()
+  const {
+    recentSearches,
+    addRecentSearch,
+    removeRecentSearch,
+    clearRecentSearches,
+  } = useRecentSearchs();
   const [loading, setLoading] = useState(false);
 
   const handleSearch = async (query: string) => {
@@ -20,13 +25,14 @@ function Explore() {
 
     setLoading(true);
 
-    const { data, error } = await supabase
-      .rpc("search_users", { search_term: query })
+    const { data, error } = await supabase.rpc("search_users", {
+      search_term: query,
+    });
 
     if (error) {
       console.error("Error searching profiles: ", error);
       setSearchResults([]);
-      setLoading(false)
+      setLoading(false);
       return;
     }
 
@@ -37,11 +43,10 @@ function Explore() {
         username: profile.username,
         avatar: profile.avatar_url,
         is_extension_active: profile.is_extension_active,
-      }))
+      })),
     );
     setLoading(false);
   };
-
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -52,41 +57,60 @@ function Explore() {
   }, [query]);
 
   return (
-    <div className="px-5 py-5 md:px-10 max-w-xl  mx-auto">
-      <Searchbar query={query} setQuery={setQuery} setShowResults={setShowResults} />
+    <div className="mx-auto max-w-xl px-5 py-5 md:px-10">
+      <Searchbar
+        query={query}
+        setQuery={setQuery}
+        setShowResults={setShowResults}
+      />
       {showResults && (
         <div className="rounded-lg border border-t-0 border-(--color-border) bg-(--color-bg-primary)">
-          <div className="custom-scroll flex flex-col gap-3 py-5  max-h-[600px] overflow-y-auto" style={{
-            scrollbarWidth: "thin",
-            scrollbarColor: "#3f3f46 transparent"
-          }}>
+          <div
+            className="custom-scroll flex max-h-150 flex-col gap-3 overflow-y-auto py-5"
+            style={{
+              scrollbarWidth: "thin",
+              scrollbarColor: "#3f3f46 transparent",
+            }}
+          >
             {loading ? (
               <p className="text-center text-sm text-gray-500">Searching...</p>
             ) : searchResults.length === 0 ? (
               <>
-                {
-                  recentSearches.length === 0 ? (
-                    <p className="text-center text-sm text-gray-500">Search for something to get started.</p>
-                  ) : (
-                    <>
-                      <div className="flex items-center justify-between px-4">
-                        <span className="text-lg text-(--color-text-primary) font-semibold" >Recent</span>
-                        <button
-                          className="hover:bg-orange-400/20 font-semibold text-center cursor-pointer text-sm text-orange-500 rounded-2xl px-3 py-1"
-                          onMouseDown={() => clearRecentSearches()}
-                        >Clear all</button>
-                      </div>
-                      {recentSearches.map((result) => (
-                        <DevloperCard key={result.id} {...result} addRecentSearch={addRecentSearch} removeRecentSearch={removeRecentSearch} />
-                      ))}
-                    </>
-                  )
-                }
-
+                {recentSearches.length === 0 ? (
+                  <p className="text-center text-sm text-gray-500">
+                    Search for something to get started.
+                  </p>
+                ) : (
+                  <>
+                    <div className="flex items-center justify-between px-4">
+                      <span className="text-lg font-semibold text-(--color-text-primary)">
+                        Recent
+                      </span>
+                      <button
+                        className="cursor-pointer rounded-2xl px-3 py-1 text-center text-sm font-semibold text-orange-500 hover:bg-orange-400/20"
+                        onMouseDown={() => clearRecentSearches()}
+                      >
+                        Clear all
+                      </button>
+                    </div>
+                    {recentSearches.map((result) => (
+                      <UserSearchCard
+                        key={result.id}
+                        {...result}
+                        addRecentSearch={addRecentSearch}
+                        removeRecentSearch={removeRecentSearch}
+                      />
+                    ))}
+                  </>
+                )}
               </>
             ) : (
               searchResults.map((result) => (
-                <DevloperCard key={result.id} {...result} addRecentSearch={addRecentSearch} />
+                <UserSearchCard
+                  key={result.id}
+                  {...result}
+                  addRecentSearch={addRecentSearch}
+                />
               ))
             )}
           </div>
