595388bf8392c3951a2c313495b8d88758334644
Roshan Patil <patilroshan9687@gmail.com>
2026-07-09 00:08:09 +0530

    refactor: update environment variable handling and improve code consistency

--- extension/streaky/src/lib/supabase.ts
+++ extension/streaky/src/lib/supabase.ts
@@ -1,13 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-import * as dotenv from "dotenv";
-import * as path from "path";
 
-dotenv.config({
-  path: path.resolve(__dirname, "../.env"),
-});
-
-const supabaseUrl = process.env.SUPABASE_URL as string;
-
-const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!,
+);
